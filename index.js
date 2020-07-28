d0c1ca29d33eb4c217294e9dd631e60f0541fef6
Reed Brooks <reedb36@gmail.com>
2020-07-28 11:46:14 -0700

    Update file structure to match Hackreactor skeleton

--- index.js
+++ index.js
@@ -2,18 +2,18 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const server = require('./server/server.js');
+const convert = require('./client/app.js');
 
 app.use(express.static('client'));
 
 //app event handler declarations
 
 //TBD: mount handler for JSON-CSV file conversion
-app.post('/', (req,res) => res.send('placeholder!'));
+app.post('/uplod_json', (req,res) => res.send('placeholder!'));
 
 
 
 //test function
-server.test();
+convert.test();
 //start listener
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
--- server/server.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  //test function
-  test: function() {
-    console.log("Server functions loaded.");
-  }
-};
