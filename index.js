5b6b8475ef7b3c67f3448bf258372c46dc7b3fb5
Reed Brooks <reedb36@gmail.com>
2020-07-28 15:53:51 -0700

    Implement key/column parsing in convertJSONtoCSV

--- index.js
+++ index.js
@@ -14,19 +14,52 @@ const app = express();
 const port = 3000;
 
 app.use(express.static('client'));
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 //function declarations
-function convertJSONtoCSV(JSON) {
-  console.log("TBD");
+function convertJSONtoCSV(json) {
+  //travel through JSON object, grabbing keys as column headers from object and any siblings/children
+  let keyList = getKeys(json);
+
+  console.log('---KEY GET TEST---');
+  console.log(keyList);
+
+  //after getting all column names, iterate through the JSON object and check each entry for its column value.
+  //set to an empty string(,) if no value found
+  let csvFormat = '';
+
+  return csvFormat;
+}
+
+//recursive helper function to get all keys from passed in object
+//keyList is in object format with value = true to handle duplicates
+function getKeys(obj) {
+  let keyList = {};
+  for(var key in obj) {
+    if(key !== 'children') {
+      keyList[key] = true;
+    }
+  }
+  for(let i = 0; i < obj.children.length; i++) {
+    console.log("Recursing...");
+    keyList = Object.assign(keyList, getKeys(obj.children[i]));
+  }
+  return keyList;
 }
 
 //app event handler declarations
 
-//TBD: mount handler for JSON-CSV file conversion
+//input will come in as req.body.inputData
 app.post('/upload_json', (req,res) => {
-  console.log(req.body);
+  //console.log(req.body.inputData);
+  let input = JSON.parse(req.body.inputData);
+  console.log(input);
+
+  //TBD: configure response information
+  let output = convertJSONtoCSV(input);
+  console.log("CSV Output:");
+  console.log(output);
   res.send('TBD');
 });
 
